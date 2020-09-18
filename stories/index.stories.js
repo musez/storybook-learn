1061365ce27d731359ef129d46e49d167717167e
王月 <920317438@qq.com>
2020-09-18 10:14:39 +0800

    :art: 格式化代码

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -8,13 +8,13 @@
 import Vue from 'vue';
 import MyButton from './Button.js';
 
-export default { title: 'Button' };
+export default {title: 'Button'};
 
 export const withText = () => '<my-button>with text</my-button>';
 
-    export const withEmoji = () => '<my-button>测试</my-button>';
+export const withEmoji = () => '<my-button>测试</my-button>';
 
 export const asAComponent = () => ({
-    components: { MyButton },
+    components: {MyButton},
     template: '<my-button :rounded="true">rounded</my-button>'
 });
